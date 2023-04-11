3a8cd2c4f49a25b061ce551f550bfe2d203ef591
VictorCiechovicz <victor09avila@hotmail.com>
2023-04-11 11:37:35 -0300

    feat: validation of forms with react-hook-forms more zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,10 +9,51 @@ import {
   InputTime
 } from './styles'
 
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+//interface NewCycleFormData {
+//  task: string
+//  minutesAmount: number
+//}
+
+//esta forma de tipagem abaixo é uma funcionalidade do zod que apartir do
+//schema de validação e ja da pronto a tipagem dos atributos
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Valor mínimo de duração é de 5 minutos')
+    .max(60, 'Valor máximo de duração é de 60 minutos')
+})
+
 export function Home() {
+  const { register, handleSubmit, watch, formState, reset } =
+    useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: '',
+        minutesAmount: 0
+      }
+    })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+    reset()
+  }
+
+  //console.log(formState.errors)
+  //para verificar as mensagens de erro que foram colocadas no schema de validação
+
+  const isTaskDisabled = watch('task')
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor=""> Vou trabalhar em </label>
 
@@ -20,7 +61,9 @@ export function Home() {
             id="task"
             list="task-sugestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
+
           <datalist id="task-sugestions">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
@@ -35,6 +78,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -47,7 +91,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={!isTaskDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
